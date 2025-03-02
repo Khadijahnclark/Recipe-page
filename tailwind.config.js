714f27153d724fabbec51cd9006dd5995a77c00f
Khadijah Clark <Khadijahnclark@gmail.com>
2025-03-02 16:16:04 -0600

    Styling pt.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@
   './src/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'stone': {
+          100: '#f3e6d8',
+          150: '#e4ded8',
+          600: '#5f574e',
+          900: ' #302d2c',          
+        },
+        'brown': {
+          800: '#854632',
+        },
+        'rose': {
+          800: '#7b284f',
+          50: '#fff5fa'
+        },
+      }
+    },
   },
   plugins: [],
 }
